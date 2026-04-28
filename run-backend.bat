f65494a9e317e0d213f55bf1294c5fdaf891eb8b
krishnamagar1 <magarkrishna448pc@gmail.com>
2026-04-28 12:42:27 +0530

    Support local database environment override

--- run-backend.bat
+++ run-backend.bat
@@ -1,5 +1,6 @@
 @echo off
 set "ROOT=%~dp0"
+if exist "%ROOT%local-env.bat" call "%ROOT%local-env.bat"
 if "%DB_USERNAME%"=="" set "DB_USERNAME=ic_user"
 if "%DB_PASSWORD%"=="" set "DB_PASSWORD=ic_password"
 "%ROOT%.tools\apache-maven-3.9.11\bin\mvn.cmd" spring-boot:run
